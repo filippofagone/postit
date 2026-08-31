7c4c7a8200edd5a13e4133a1f1b625211917c749
Post-It Builder <filippofagone@users.noreply.github.com>
2026-08-31 23:21:58 +0000

    v6.7: tutorial guidato da Postino (riflettore sugli elementi veri, lapis/emoji/colori, giro del gruppo, sonno con zzz), tastiera adjustResize, header largo

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v52";
+const CACHE = "postit-v53";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
