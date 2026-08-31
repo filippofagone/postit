c061c532defece318e4816812621cce5c1c3d87a
Post-It Builder <filippofagone@users.noreply.github.com>
2026-08-31 10:57:57 +0000

    v5.9: resa telefono come PC (geometricPrecision + contain layout)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v44";
+const CACHE = "postit-v45";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
