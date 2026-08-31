aaa396df317734f8a60bb138aa7091e44b2bee44
Post-It Builder <filippofagone@users.noreply.github.com>
2026-08-31 10:31:59 +0000

    v5.7: identità unica, ruoli+permessi, Centro Assistenza, chat+foto, annunci, DM, navigazione, intro definitiva

--- docs/OneSignalSDKWorker.js
+++ docs/OneSignalSDKWorker.js
@@ -1,3 +1,3 @@
-// Post-It build v5.0
+// Post-It build v5.7
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 importScripts("sw.js");
--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v42";
+const CACHE = "postit-v43";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
