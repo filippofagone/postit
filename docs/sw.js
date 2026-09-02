80e63877c134823abacdc8fce4e5a870ca1c1bb5
Post-It Builder <filippofagone@users.noreply.github.com>
2026-09-02 14:47:07 +0000

    v8.0: Coach ad angolino (schermo pulito, niente timer), tastiera domata, arte SVG senza emoji, Postie 4.0 (10 espressioni, spirali vere, occhiali sugli occhi, sonno rifatto), lezioni per ogni sezione, volo a casa, header sistemato, ripristino SureModal/SectionHint/MascotOverlay

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v62";
+const CACHE = "postit-v63";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
