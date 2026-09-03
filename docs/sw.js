fca0c1344175eb2601a3097778b0a4ba9e13e73e
Post-It Builder <filippofagone@users.noreply.github.com>
2026-09-03 15:11:38 +0000

    v10.3: puntino piazzato da una sonda renderizzata sul dispositivo (linea di base vera del telefono), pieghe della lavagna identiche alle caselle

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v79";
+const CACHE = "postit-v80";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
