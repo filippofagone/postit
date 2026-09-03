8b8a804c9d65c08f4603e4acca876fd8b639b0d0
Post-It Builder <filippofagone@users.noreply.github.com>
2026-09-03 09:43:46 +0000

    v9.6: Modifica profilo (nome, emoji, colore) al posto di Cambia utente; pagina profilo col colore scelto; aggiornamento propagato a tutti i gruppi

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v69";
+const CACHE = "postit-v70";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
