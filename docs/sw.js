76589d7e5cda31994d3363154f61fca3fc1a828e
Post-It Builder <filippofagone@users.noreply.github.com>
2026-09-03 07:38:15 +0000

    v9.0: tutorial e Postino estratti nel pacchetto, sincronizzazione Supabase (gruppi+contenuti, tempo reale, ingresso col codice dal cloud), pallino di stato, aggancio OneSignal per utente

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v63";
+const CACHE = "postit-v64";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
