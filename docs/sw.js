065bb6f909c5068fbfa35d19ef2792fd79304eb3
Post-It Builder <filippofagone@users.noreply.github.com>
2026-09-03 21:57:36 +0000

    v11.7: avatar e saluto risolti dall'impronta del profilo in entrambe le schermate del gruppo

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v94";
+const CACHE = "postit-v95";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
