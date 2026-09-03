05882505c5e37fda7a0a556671abd38b61f94229
Post-It Builder <filippofagone@users.noreply.github.com>
2026-09-03 10:10:21 +0000

    v9.9: intro con partitura unica (pin 0,5s dopo la melodia, stesso orologio audio), puntino della P. in riga e ravvicinato, pieghe senza triangolino bianco in tutta l'app

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v75";
+const CACHE = "postit-v76";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
