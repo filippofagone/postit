589423bad75926b5a5ab7fe32fe6e865afed2377
Post-It Builder <filippofagone@users.noreply.github.com>
2026-09-03 10:22:15 +0000

    v10.0: piega vera (angolo ritagliato col vuoto dietro, risvolto nello stesso colore più scuro), puntino sul vertice del rettangolo della P, pin subito dopo l'ultima nota

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v76";
+const CACHE = "postit-v77";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
