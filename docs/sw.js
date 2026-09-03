7ec6b9e408151f8de63e0b5515fb0f3404971a7c
Post-It Builder <filippofagone@users.noreply.github.com>
2026-09-03 09:16:48 +0000

    v9.5: campanellini in-app + avviso a schermo su ogni notifica (iPhone/computer inclusi)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v68";
+const CACHE = "postit-v69";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
