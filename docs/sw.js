311ec09b44f02b273a219c2e0cb813cd5f16c838
Post-It Builder <filippofagone@users.noreply.github.com>
2026-09-03 21:57:01 +0000

    v11.6: fusione dei contenuti a ogni deposito (messaggi/annunci non spariscono piu), identita nel gruppo risolta per impronta del profilo (avatar e saluto di ciascuno)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postit-v93";
+const CACHE = "postit-v94";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
